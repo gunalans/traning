9f8d895a54c7f116ae452ce81a07fadd1f5ac6d2
gunalans <93249665+gunalans@users.noreply.github.com>
2021-10-27 15:06:02 +0530

    Add files via upload

---
+++ FIRST.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// variables here
+int guna=0;
+
+//***************************main**********************
+void main()
+{
+clrscr();
+printf("hello");
+printf("%d",guna);
+getch();
+}
+//*****************************end**********************
