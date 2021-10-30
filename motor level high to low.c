26934ff124b9a3455560631dd2e9a4c39bf5122a
gunalans <93249665+gunalans@users.noreply.github.com>
2021-10-30 12:00:46 +0530

    Add files via upload

---
+++ motor level high to low.c
@@ -0,0 +1,50 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int a,i,n,j,k;
+int main()
+{
+    printf("enter tank level1 :");
+    scanf("%d", &a);
+    printf("enter max level :");
+    scanf("%d", &n);
+    for(i=a;i<=n;i++)
+ {
+     printf("\ncurrent level: %d",i);
+     if(i==n)
+     {
+     printf("tank is full");
+     }
+ }
+  for(j=n;j>=0;j--)
+  {
+      printf("\ncurrent level: %d",j);
+      if(j==0)
+      {
+          printf("\ntank is empty");
+    
+      }
+      
+      
+  }
+  for(k=0;k<=5;k++)
+  {
+    printf("\ncurrent level:%d",k);
+    if(k==5)
+    {
+        printf("\ntank is full");
+    }
+  }
+  return 0;
+     
+}
+
+  
+
+
